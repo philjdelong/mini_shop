72b487ae5a84ed1d2909dd114d8b56e0c367e4ad
Phil DeLong <philjdelong@gmail.com>
2019-11-29 16:22:34 -0700

    add test route and controller for new merchant

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,4 +3,7 @@ class MerchantsController < ApplicationController
   def index
     @merchants = Merchant.all
   end
+
+  def new
+  end
 end
---
+++ spec/merchants/new_merchant_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "As a user" do
+  describe "when i visit the merchants index page", type: :feature do
+
+    it "i can click link to create a new merchant" do
+      visit "/merchants"
+      expect(page).to_not have_content("Phil DeLong")
+      click_on "New Merchant"
+      expect(current_path).to eq("/merchants/new")
+
+      fill_in 'Name', with: "Phil DeLong"
+      fill_in 'Address', with: "3102 N. Race St."
+      fill_in 'City', with: "Denver"
+      fill_in 'State', with: "CO"
+      fill_in 'Zip', with: 80205
+
+      click_on "Submit"
+      expect(current_path).to eq("/merchants")
+
+      expect(page).to have_content("Phil DeLong")
+    end
+  end
+end
