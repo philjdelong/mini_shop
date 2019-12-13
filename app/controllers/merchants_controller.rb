f53675759d5ae99837058daaaeab88a3cf751c62
Phil DeLong <philjdelong@gmail.com>
2019-12-12 19:21:29 -0700

    Add controller method for update merchant. Pass test

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -12,6 +12,20 @@ class MerchantsController < ApplicationController
     redirect_to "/merchants"
   end
 
+  def show
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(merchant_params)
+    redirect_to "/merchants/#{merchant.id}"
+  end
+
   private
 
     def merchant_params
